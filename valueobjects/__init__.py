6b34645dee920fd7080b6b1677c60895121510ed
Javier Parada <javierparadadev@gmail.com>
2022-12-31 12:33:21 +0100

    update init

---
+++ valueobjects/__init__.py
@@ -0,0 +1,6 @@
+from valueobjects.value_object import ValueObject
+
+from valueobjects.numbers.int import Int
+from valueobjects.numbers.natural_number import NaturalNumber
+from valueobjects.numbers.positive_int import PositiveInt
+from valueobjects.numbers.negative_int import NegativeInt
