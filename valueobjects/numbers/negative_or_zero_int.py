02452ea92ce931f9b8ef93691dec8b68773783d8
Javier Parada <javierparadadev@gmail.com>
2022-12-31 14:00:15 +0100

    add negative or zero

---
+++ valueobjects/numbers/negative_or_zero_int.py
@@ -0,0 +1,13 @@
+from valueobjects.errors.ValueObjectError import ValueObjectError
+from valueobjects.numbers.int import Int
+
+
+class NegativeOrZeroInt(Int):
+
+    def __init__(self, value: int):
+        super().__init__(value)
+        self._validate(value)
+
+    def _validate(self, value):
+        if value > 0:
+            raise ValueObjectError('Value must be less or equals to 0.')
