1282df25cadc000bbe5a9ea72f461511198cb90a
Javier Parada <javierparadadev@gmail.com>
2023-01-14 10:12:07 +0100

    Add nullable negative int tests

---
+++ tests/unit/nullable_negative_int_test.py
@@ -0,0 +1,59 @@
+import unittest
+
+from pyvalueobjects.errors.ValueObjectError import ValueObjectError
+from pyvalueobjects.numbers.nullable_negative_int import NullableNegativeInt
+
+
+class TestNullableNegativeIntValueObject(unittest.TestCase):
+    def test_value_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt, 39)
+
+    def test_from_string_method_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt.from_str, '39')
+
+    def test_from_float_method_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt.from_float, 39.0)
+
+    def test_zero_value_return_input_value(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt, 0)
+
+    def test_zero_from_string_method_returns_int(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt.from_str, '0')
+
+    def test_zero_from_float_method_returns_int(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt.from_float, 0.0)
+
+    def test_negative_value_return_input_value(self):
+        vo = NullableNegativeInt(-39)
+        self.assertEqual(-39, vo.value())
+
+    def test_from_string_method_returns_negative_int(self):
+        vo = NullableNegativeInt.from_str('-39')
+        self.assertEqual(-39, vo.value())
+
+    def test_from_float_method_returns_negative_int(self):
+        vo = NullableNegativeInt.from_float(-39.0)
+        self.assertEqual(-39, vo.value())
+
+    def test_from_string_with_not_numerical_format_raise_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt.from_str, 'patata')
+
+    def test_from_float_with_decimals_raise_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt.from_float, 39.1)
+
+    def test_from_str_number_with_no_decimals(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt.from_str, '39.0')
+
+    def test_from_str_negative_number_with_no_decimals(self):
+        vo = NullableNegativeInt.from_str('-39.0')
+        self.assertEqual(-39, vo.value())
+
+    def test_from_str_number_with_decimals_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt.from_float, '39.1')
+
+    def test_from_str_negative_number_with_decimals_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeInt.from_float, '-39.1')
+
+    def test_none_value_return_none(self):
+        vo = NullableNegativeInt(None)
+        self.assertEqual(None, vo.value())
