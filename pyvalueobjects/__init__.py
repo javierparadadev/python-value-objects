64b86c1404107750e3c784ab69ef25999f3389dd
Javier Parada <javierparadadev@gmail.com>
2023-01-15 10:42:28 +0100

    Add nullable negative int
    Close #14

--- pyvalueobjects/__init__.py
+++ pyvalueobjects/__init__.py
@@ -14,5 +14,6 @@ from pyvalueobjects.numbers.int import Int
 from pyvalueobjects.numbers.nullable_int import NullableInt
 from pyvalueobjects.numbers.positive_int import PositiveInt
 from pyvalueobjects.numbers.negative_int import NegativeInt
+from pyvalueobjects.numbers.nullable_negative_int import NullableNegativeInt
 from pyvalueobjects.numbers.positive_or_zero_int import PositiveOrZeroInt
 from pyvalueobjects.numbers.negative_or_zero_int import NegativeOrZeroInt
