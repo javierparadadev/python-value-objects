7b5889e561f5569b1f94f68c95527b8e602bcb16
Javier Parada <javierparadadev@gmail.com>
2023-01-19 13:48:57 +0100

    Add nullable_negative_or_zero_int_test.py #15

---
+++ tests/unit/nullable_negative_or_zero_int_test.py
@@ -0,0 +1,66 @@
+import unittest
+
+from pyvalueobjects.errors.ValueObjectError import ValueObjectError
+from pyvalueobjects.numbers.nullable_negative_or_zero_int import NullableNegativeOrZeroInt
+
+
+class TestNullableNegativeOrZeroIntValueObject(unittest.TestCase):
+    def test_value_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeOrZeroInt, 39)
+
+    def test_from_string_method_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeOrZeroInt.from_str, '39')
+
+    def test_from_float_method_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeOrZeroInt.from_float, 39.0)
+
+    def test_zero_value_return_input_value(self):
+        vo = NullableNegativeOrZeroInt(0)
+        self.assertEqual(0, vo.value())
+
+    def test_zero_from_string_method_returns_int(self):
+        vo = NullableNegativeOrZeroInt.from_str('0')
+        self.assertEqual(0, vo.value())
+
+    def test_zero_from_float_method_returns_int(self):
+        vo = NullableNegativeOrZeroInt.from_float(0.0)
+        self.assertEqual(0, vo.value())
+
+    def test_zero_from_string_with_float_format_method_returns_int(self):
+        vo = NullableNegativeOrZeroInt.from_str('0.0')
+        self.assertEqual(0, vo.value())
+
+    def test_negative_value_return_input_value(self):
+        vo = NullableNegativeOrZeroInt(-39)
+        self.assertEqual(-39, vo.value())
+
+    def test_from_string_method_returns_negative_int(self):
+        vo = NullableNegativeOrZeroInt.from_str('-39')
+        self.assertEqual(-39, vo.value())
+
+    def test_from_float_method_returns_negative_int(self):
+        vo = NullableNegativeOrZeroInt.from_float(-39.0)
+        self.assertEqual(-39, vo.value())
+
+    def test_from_string_with_not_numerical_format_raise_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeOrZeroInt.from_str, 'patata')
+
+    def test_from_float_with_decimals_raise_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeOrZeroInt.from_float, 39.1)
+
+    def test_from_str_number_with_no_decimals(self):
+        self.assertRaises(ValueObjectError, NullableNegativeOrZeroInt.from_str, '39.0')
+
+    def test_from_str_negative_number_with_no_decimals(self):
+        vo = NullableNegativeOrZeroInt.from_str('-39.0')
+        self.assertEqual(-39, vo.value())
+
+    def test_from_str_number_with_decimals_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeOrZeroInt.from_float, '39.1')
+
+    def test_from_str_negative_number_with_decimals_raises_error(self):
+        self.assertRaises(ValueObjectError, NullableNegativeOrZeroInt.from_float, '-39.1')
+
+    def test_none_value_return_none(self):
+        vo = NullableNegativeOrZeroInt(None)
+        self.assertEqual(None, vo.value())
