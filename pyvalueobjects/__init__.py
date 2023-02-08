555bc0b4691f728ed8f432f29411de51d9726e9a
Javier Parada <javierparadadev@gmail.com>
2023-02-08 18:20:52 +0100

    Export String. Close #1

--- pyvalueobjects/__init__.py
+++ pyvalueobjects/__init__.py
@@ -19,3 +19,9 @@ from pyvalueobjects.numbers.positive_or_zero_int import PositiveOrZeroInt
 from pyvalueobjects.numbers.nullable_positive_or_zero_int import NullablePositiveOrZeroInt
 from pyvalueobjects.numbers.negative_or_zero_int import NegativeOrZeroInt
 from pyvalueobjects.numbers.nullable_negative_or_zero_int import NullableNegativeOrZeroInt
+
+# -----------------------------
+# ---------- Strings ----------
+# -----------------------------
+
+from pyvalueobjects.strings.string import String
