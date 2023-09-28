b4e1f0ec7aee0af1248c93981124ea3ea283e085
Javier Parada <javierparadadev@gmail.com>
2023-09-28 12:03:06 +0900

    Export module #25

--- pyvalueobjects/__init__.py
+++ pyvalueobjects/__init__.py
@@ -41,4 +41,5 @@ from pyvalueobjects.dates.isodate import IsoDate
 # -----------------------------
 
 from pyvalueobjects.security.cve import Cve
+from pyvalueobjects.security.nullable_cve import NullableCve
 from pyvalueobjects.security.cpe import Cpe
