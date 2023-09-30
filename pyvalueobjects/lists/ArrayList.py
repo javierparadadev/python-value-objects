ea8810c4a23665c740079b5d4e90b4a7ecfcd54c
Javier Parada <javierparadadev@gmail.com>
2023-09-30 16:06:14 +0900

    Add array list

---
+++ pyvalueobjects/lists/ArrayList.py
@@ -0,0 +1,22 @@
+from typing import TypeVar, Generic
+
+from pyvalueobjects.abstract.valueobject import ValueObject
+from pyvalueobjects.errors.ValueObjectError import ValueObjectError
+
+T = TypeVar('T')
+
+
+def ArrayList(cls):
+    class _ArrayList(ValueObject):
+
+        def __init__(self, values: list[cls]):
+            super().__init__(values)
+            self._sub_value_objects = [cls(value) for value in values]
+
+        def _validate(self, value):
+            super()._validate(value)
+            input_type = type(value)
+            if input_type != list:
+                raise ValueObjectError(f'Input type should be: list.')
+
+    return _ArrayList
