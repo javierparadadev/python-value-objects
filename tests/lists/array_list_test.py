5bbfc2759fd991f32b19338d29ec3d0a3f0cedf8
Javier Parada <javierparadadev@gmail.com>
2023-10-01 16:06:36 +0900

    Add array list tests

---
+++ tests/lists/array_list_test.py
@@ -0,0 +1,31 @@
+import unittest
+
+from pyvalueobjects.lists.ArrayList import ArrayList
+from pyvalueobjects.numbers.int import Int
+from pyvalueobjects.strings.string import String
+
+
+class TestArrayListValueObject(unittest.TestCase):
+    def test_value_return_input_value(self):
+        vo = ArrayList(Int)([39])
+        self.assertEqual([39], vo.value())
+
+    def test_value_return_input_string_value(self):
+        vo = ArrayList(String)(['a'])
+        self.assertEqual(['a'], vo.value())
+
+    def test_value_return_empty_array(self):
+        vo = ArrayList(Int)([])
+        self.assertEqual([], vo.value())
+
+    def test_value_return_empty_string_array(self):
+        vo = ArrayList(String)([])
+        self.assertEqual([], vo.value())
+
+    def test_value_return_multiple_input_values(self):
+        vo = ArrayList(Int)([39, 100])
+        self.assertEqual([39, 100], vo.value())
+
+    def test_value_return_input_multiple_string_values(self):
+        vo = ArrayList(String)(['a', 'b'])
+        self.assertEqual(['a', 'b'], vo.value())
