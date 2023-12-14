f33bc62e9637af3fa506320e0f199bcf72e448e2
Javier Parada <javierparadadev@gmail.com>
2023-12-14 23:08:52 +0900

    Add isodate hash tests #30

--- tests/dates/isodate_test.py
+++ tests/dates/isodate_test.py
@@ -6,6 +6,24 @@ from pyvalueobjects.errors.ValueObjectError import ValueObjectError
 
 
 class TestIsoDateValueObject(unittest.TestCase):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self._cls = IsoDate
+
+    def test_vo_equal_hash(self):
+        the_date = datetime.datetime.now().isoformat()
+        original_vo_hash = hash(self._cls(the_date))
+        equal_vo_hash = hash(self._cls(the_date))
+        self.assertEqual(original_vo_hash, equal_vo_hash)
+
+    def test_vo_different_hash(self):
+        the_date = datetime.datetime.now().isoformat()
+        one_day_ago = (datetime.datetime.now() - datetime.timedelta(days=1)).isoformat()
+        original_vo_hash = hash(self._cls(the_date))
+        not_equal_vo_hash = hash(self._cls(one_day_ago))
+        self.assertNotEqual(original_vo_hash, not_equal_vo_hash)
+
     def test_value_return_input_value(self):
         the_date = datetime.datetime.now().isoformat()
         vo = IsoDate(the_date)
