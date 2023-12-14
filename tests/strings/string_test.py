ccd589259d9709f12579af8cf4e7a776d2585db5
Javier Parada <javierparadadev@gmail.com>
2023-12-14 23:47:10 +0900

    Add string VO hash. #30

--- tests/strings/string_test.py
+++ tests/strings/string_test.py
@@ -5,6 +5,21 @@ from pyvalueobjects.strings.string import String
 
 
 class TestStringValueObject(unittest.TestCase):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self._cls = String
+
+    def test_vo_equal_hash(self):
+        original_vo_hash = hash(self._cls('patata'))
+        equal_vo_hash = hash(self._cls('patata'))
+        self.assertEqual(original_vo_hash, equal_vo_hash)
+
+    def test_vo_different_hash(self):
+        original_vo_hash = hash(self._cls('patata'))
+        not_equal_vo_hash = hash(self._cls('tomato'))
+        self.assertNotEqual(original_vo_hash, not_equal_vo_hash)
+
     def test_value_return_input_value(self):
         vo = String('')
         self.assertEqual('', vo.value())
