b7f44350ffc806e352e7febd792fc53b13f1d1a4
Javier Parada <javierparadadev@gmail.com>
2023-12-15 09:04:36 +0900

    Add string VO EQ method. #31

--- tests/strings/uuid4_test.py
+++ tests/strings/uuid4_test.py
@@ -21,6 +21,16 @@ class TestUuid4ValueObject(unittest.TestCase):
         not_equal_vo_hash = hash(self._cls('c8ab500c-bd40-4bb2-8feb-efb22404f404'))
         self.assertNotEqual(original_vo_hash, not_equal_vo_hash)
 
+    def test_vo_equality(self):
+        original_vo = self._cls('e88cf76c-904b-4f4f-81ee-8524a60bf5d0')
+        equal_vo = self._cls('e88cf76c-904b-4f4f-81ee-8524a60bf5d0')
+        self.assertEqual(original_vo, equal_vo)
+
+    def test_vo_different_equality(self):
+        original_vo = self._cls('e88cf76c-904b-4f4f-81ee-8524a60bf5d0')
+        different_vo = self._cls('c8ab500c-bd40-4bb2-8feb-efb22404f404')
+        self.assertNotEqual(original_vo, different_vo)
+
     def test_value_return_input_value(self):
         the_uuid = str(uuid.uuid4())
         vo = Uuid4(the_uuid)
